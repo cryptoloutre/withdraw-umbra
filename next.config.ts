038dc6ec085e6bc154cb3f3b46e73cce803434f5
cryptoloutre <miningcryptomonnaie@gmail.com>
2025-09-16 08:16:56 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-   rules: {
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off'
-  },
 }
 
 export default nextConfig
